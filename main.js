b57d13d04c8a40d695685ea6c5d2816782d97639
mac-handel999 <fabikechukwuemeana@gmail.com>
2024-08-19 21:05:53 +0100

    update (added password, alto. jpg, and registration form page)

--- main.js
+++ main.js
@@ -16,10 +16,37 @@
   
   
   
-   function navigateToLink(selectElement) {
-            var url = selectElement.value;
-            if (url) {
-                window.open(url, '_blank');
-            }
-        }
-  
+          
+
+        
+        
+
+  const selectElem = document.querySelector("select");
+
+  selectElem.addEventListener('change', () => {
+    function checkPassword() {
+      var password = prompt("Please enter the password:");
+      if (password === "Sing@Choir") {
+        alert("Access granted!");
+        
+        // Call navigateToLink function to open the selected link
+        
+        navigateToLink(selectElem);
+        
+        
+      } else {
+        alert("Access denied!");
+        selectElem.value = ""; // Reset the selection
+      }
+    }
+
+    function navigateToLink(selectElem) {
+      var url = selectElem.value; // Get the selected value and trim any whitespace
+      if (url) {
+        window.open(url, '_blank'); // Open the URL in a new tab
+      }
+    }
+
+    // Call checkPassword when the user makes a selection
+    checkPassword();
+  });
